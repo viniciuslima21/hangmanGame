ddcbb09c357169fa9aafee28e06f569916272917
Vinícius Lima <viniciuslimaandre@hotmail.com>
2022-03-23 19:20:51 -0300

    refactoring the code

--- js/script.js
+++ js/script.js
@@ -17,121 +17,106 @@ const wrong = new Audio('./assets/audio/wrong.mp3')
 const lose = new Audio('./assets/audio/lose.mp3')
 const win = new Audio('./assets/audio/win.mp3')
 
-const letters = []
-
 const numberOfChances = 3
+const hangedColor = 'color: #FF4040'
+const timeoutToShowAlert = 500
+
+// GET
+const getHangman = () => JSON.parse(localStorage.getItem('hangman'))
+
+// SET
+const setHangman = hangman => localStorage.setItem('hangman', JSON.stringify(hangman))
 
 // Selecting random word
 const randomNumberOfWord = () => Math.floor(Math.random() * words.length - 1)
 
-// GETS
-const getWord = () => JSON.parse(localStorage.getItem('word'))
-
-const getWordSplitted = () => JSON.parse(localStorage.getItem('wordSplitted'))
-
-const getLetters = () => JSON.parse(localStorage.getItem('letters'))
-
-const getAnswer = () => JSON.parse(localStorage.getItem('awnser'))
-
-const getChances = () => JSON.parse(localStorage.getItem('chances'))
-
-// SETS
-const setWord = word => localStorage.setItem('word', JSON.stringify(word))
-
-const setWordSplitted = word => localStorage.setItem('wordSplitted', JSON.stringify(word))
-
-const setLetters = letter => localStorage.setItem('letters', JSON.stringify(letter))
-
-const setAwnser = awnser => localStorage.setItem('awnser', JSON.stringify(awnser))
-
-const setChances = chance => localStorage.setItem('chances', JSON.stringify(chance))
-
 // Selecting word
-const selectWord = () => { return words[randomNumberOfWord()] }
-
-// Generating bars, tip and length
-const generateAll = (letters, tip, length) => {
-    const bars = new Array(letters.length).fill('_')
-
-    setAwnser(bars)
-    wordDisplay.innerHTML = bars.join(' ')
-    tipDisplay.innerHTML = tip
-    numberOfLettersDisplay.innerHTML = length
-}
+const selectWord = () => words[randomNumberOfWord()]
 
 // Verifing if the letter has already been informed
 const letterHasAlreadyBeenInformed = letter => {
-    const letterHasAlreadyBeenInformed = getLetters()
+    const { letters } = getHangman()
 
-    if (letterHasAlreadyBeenInformed.length > 0) {
-        for (let i of letterHasAlreadyBeenInformed) {
+    if (letters.length > 0) {
+        for (let i of letters)
             if (i === letter) return true
-        }
     }
     return false
 }
 
 // Verifing if the letter exist
 const letterExist = letter => {
-    const wordSplitted = getWordSplitted()
-    const { word } = getWord()
-    const style = 'color: #FF4040'
-    let chances = getChances()
+    const hangman = getHangman()
 
-    for (let i of wordSplitted) {
+    for (let i of hangman.wordSplitted) {
         if (i === letter) return correct.play()
     }
 
-    if (chances === 3) {
+    if (hangman.chances === 3) {
         wrong.play()
-        chance2.style = style
-        chances = 2
-    } else if (chances === 2) {
+        chance2.style = hangedColor
+        hangman.chances = 2
+    } else if (hangman.chances === 2) {
         wrong.play()
-        chance1.style = style
-        chances = 1
-    }
-    else if (chances === 1) {
-        chance0.style = style
-        chances = 0
+        chance1.style = hangedColor
+        hangman.chances = 1
+    } else if (hangman.chances === 1) {
+        chance0.style = hangedColor
+        hangman.chances = 0
 
         lose.play()
-        setTimeout(() => {
-            alert('Enforcado! Você errou a letra pela 3ª vez. 😞 \nA resposta certa era: ' + word)
-            location.reload()
-        }, 500)
+        setTimeout(() => showAlert('lostAllChances', hangman.word), timeoutToShowAlert)
     }
-    setChances(chances)
-    chancesDisplay.innerHTML = chances
+    chancesDisplay.innerHTML = hangman.chances
+    setHangman(hangman)
+}
+
+// Update all displays
+const updateDisplays = (hangman) => {
+    wordDisplay.innerHTML = hangman.awnser.join(' ')
+    tipDisplay.innerHTML = hangman.tip
+    numberOfLettersDisplay.innerHTML = hangman.awnser.length
+    chancesDisplay.innerHTML = hangman.chances
+    lettersDisplay.innerHTML = hangman.letters.length ? hangman.letters : '-'
 }
 
 // Updating letters on display
 const updateLettersOnDisplay = () => {
-    const letters = getLetters()
+    const { letters } = getHangman()
 
     lettersDisplay.innerHTML = ''
     lettersDisplay.innerHTML = letters.length > 1 ? letters.join(' - ') : letters
 }
 
-// Cleaning displays
-const cleanDisplays = () => {
-    wordDisplay.innerHTML = ''
-    tipDisplay.innerHTML = ''
-    lettersDisplay.innerHTML = '-'
+// Showing alerts
+const showAlert = (msg, word) => {
+    if (msg === 'wordWrong')
+        alert('Você errou! 😞 \nA resposta certa era: ' + word)
+    else if (msg === 'lostAllChances')
+        alert('Enforcado! Você errou a letra pela 3ª vez. 😞 \nA palavra era: ' + word)
+    else
+        alert('Parabéns! Você acertou! 🥳')
+
+    location.reload()
 }
 
 // Starting
 const init = () => {
-    const selectedWord = selectWord()
-    const { word, tip } = selectedWord
+    const hangman = {}
+    const { word, tip } = selectWord()
     const allLettersFromWord = word.split('')
+    const bars = new Array(allLettersFromWord.length).fill('_')
 
-    cleanDisplays()
-    setLetters([])
-    setWord(selectedWord)
-    setWordSplitted(allLettersFromWord)
-    setChances(numberOfChances)
-    generateAll(allLettersFromWord, tip, word.length)
+    hangman.word = word
+    hangman.tip = tip
+    hangman.letters = []
+    hangman.wordSplitted = allLettersFromWord
+    hangman.chances = numberOfChances
+    hangman.awnser = bars
+
+    localStorage.removeItem('hangman')
+    setHangman(hangman)
+    updateDisplays(hangman)
 }
 
 init()
@@ -141,44 +126,34 @@ const showLetter = e => {
     e.preventDefault()
 
     const letterOrWord = inputLetter.value.trim().toLowerCase()
-    const { word } = getWord()
-    const wordSplitted = getWordSplitted()
-    const awnser = getAnswer()
+    const { word, wordSplitted } = getHangman()
 
     if (letterOrWord === '') return alert('ATENÇÃO: Você precisa informar uma letra ou a palavra para continuar.')
-
     inputLetter.value = ''
 
     if (letterOrWord.length === 1) {
         if (!letterHasAlreadyBeenInformed(letterOrWord)) {
             letterExist(letterOrWord)
-
-            letters.push(letterOrWord)
-            setLetters(letters)
-            updateLettersOnDisplay()
+            const hangman = getHangman()
 
             for (let i = 0; i < wordSplitted.length; i++) {
                 if (wordSplitted[i] === letterOrWord)
-                    awnser[i] = letterOrWord
+                    hangman.awnser[i] = letterOrWord
             }
+            wordDisplay.innerHTML = hangman.awnser.join(' ')
 
-            setAwnser(awnser)
-            wordDisplay.innerHTML = awnser.join(' ')
+            hangman.letters.push(letterOrWord)
+            setHangman(hangman)
+            updateLettersOnDisplay()
         } else
             alert('ATENÇÃO: Essa letra já foi informada!')
     } else {
         if (word === letterOrWord) {
             win.play()
-            setTimeout(() => {
-                alert('Parabéns! Você acertou! 🥳')
-                location.reload()
-            }, 500)
+            setTimeout(() => showAlert('win'), timeoutToShowAlert)
         } else {
             lose.play()
-            setTimeout(() => {
-                alert('Você errou! 😞 \nA resposta certa era: ' + word)
-                location.reload()
-            }, 500)
+            setTimeout(() => showAlert('wordWrong', word), timeoutToShowAlert)
         }
     }
 }
